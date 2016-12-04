50e4562c62b1729c1bd8fe0147cff3614f554ed5
Alexander Korling <lermannen@gmail.com>
2016-12-04 18:14:44 +0100

    Day 4 part 2

--- lib/day04.ex
+++ lib/day04.ex
@@ -4,11 +4,11 @@ defmodule Day04 do
     sector = String.slice(string, -10..-8)
     checksum = String.slice(string, -6..-2)
    # IO.puts "#{string}: #{name} | #{sector} | #{checksum}"
-    %{name: String.replace(name, "-", ""), sector: sector, checksum: checksum}
+    %{name: name, sector: String.to_integer(sector), checksum: checksum}
   end
 
   def real_room?(%{name: name, checksum: checksum}) do
-    x = Enum.sort(String.graphemes(name))
+    x = Enum.sort(String.graphemes(String.replace(name, "-", "")))
     map = Enum.reduce(x, %{}, fn(x, acc) -> Map.update(acc, x, 1, &(&1 + 1)) end)
     sorted = Enum.sort(map, fn({k1, x}, {k2, y}) -> case x == y do; true -> k1 < k2; false -> x > y; end; end)
     real_checksum = Enum.join(Enum.map(Enum.take(sorted, 5), fn({k, _v}) -> k end))
@@ -17,7 +17,7 @@ defmodule Day04 do
 
   def sector(room = %{sector: sector}) do
     case real_room?(room) do
-      true -> String.to_integer(sector)
+      true -> sector
       false -> 0
     end
   end
@@ -26,4 +26,29 @@ defmodule Day04 do
     rooms = String.split(input, "\n")
     Enum.reduce(rooms, 0, fn(x, acc) -> sector(transform(x)) + acc end)
   end
+
+  def solve2(input) do
+    rooms = Enum.reject(String.split(input, "\n"), fn(x) -> !real_room?(transform(x)) end)
+    decoded = Enum.map(rooms, fn(r) -> %{decoded: decrypt(transform(r)), name: r} end)
+#    IO.inspect decoded
+    filter(decoded)
+  end
+
+  defp filter([]), do: :ok
+  defp filter([%{decoded: d, name: n} | rest]) do
+    case String.starts_with?(d, "north") do
+      true -> IO.puts "#{d}, originally: #{n}"
+      false -> :ok
+    end
+    filter(rest)
+  end
+  defp filter(_), do: :ok
+
+  def decrypt(%{name: name, sector: sector}) do
+    Enum.join(Enum.map(String.codepoints(name), fn(x) -> Day04.decode(x, rem(sector, 26)) end))
+  end
+
+  def decode("-", _), do: " "
+  def decode(<< c >>, value) when c + value > 122, do: << c + value - 26 >>
+  def decode(<< c >>, value), do: << c + value >>
 end
--- test/day04_test.exs
+++ test/day04_test.exs
@@ -3,7 +3,7 @@ defmodule Day04Test do
   doctest Day04
 
   test "parses input" do
-    expected = %{name: "aaaaabbbzyx", sector: "123", checksum: "abxyz"}
+    expected = %{name: "aaaaa-bbb-z-y-x", sector: 123, checksum: "abxyz"}
     assert expected == Day04.transform("aaaaa-bbb-z-y-x-123[abxyz]")
   end
 
@@ -12,9 +12,15 @@ defmodule Day04Test do
     assert Day04.real_room?(data)
   end
 
+  test "decrypt" do
+    data = Day04.transform("aaaaa-bbb-z-y-x-123[abxyz]")
+    assert "very encrypted name" == Day04.decrypt(%{name: "qzmt-zixmtkozy-ivhz", sector: 343})
+  end
+
   test "solve" do
     {:ok, file} = File.read("lib/day04.input")
     part1 = Day04.solve(String.trim(file))
+    Day04.solve2(String.trim(file))
     IO.puts "Sum of sectors: #{part1}"
   end
 end
